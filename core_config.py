d833ddcdd6e621c7ff3c67e2e54803b7a0a48d6d
REUSSITESS <contact@reussitess.fr>
2026-01-04 14:05:54 -0400

    💎 CRITICAL: Migration vers l'adresse Maître 0xB375... (BOUDOUM)

---
+++ core_config.py
@@ -0,0 +1,17 @@
+# 📜 CONFIGURATION SOURCE MISE À JOUR - REUSSITESS©
+# ORIGINE: Guadeloupe, "Terres De Champions Positivité à l'infini Boudoum"
+
+PROJECT_DNA = {
+    "name": "Reussitess©",
+    "origin": "Guadeloupe",
+    "slogan": "Terres De Champions Positivité à l'infini Boudoum",
+    "total_supply": 1000000000,
+    "master_address": "0xB37531727fC07c6EED4f97F852A115B428046EB2",
+    "initial_owner": "0x69f42aa645a43a84e1143d416a4c81a88df01549"
+}
+
+AUTHORIZED_COUNTRIES = [
+    "France", "Belgique", "Italie", "Allemagne", "Suède", 
+    "Singapour", "Australie", "Espagne", "Brésil", "Royaume-Uni", 
+    "Inde", "Nouvelle-Zélande", "États-Unis", "Canada"
+]
