c91262e3c29a44d36554b3faad84769748973cfe
Reussitess30 <influenceur@reussitess.fr>
2026-03-14 14:00:29 -0400

    🛍️ PAAPI SDK + supprime test-env

--- pages/api/test-env.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.json({
-    ak: process.env.AMAZON_ACCESS_KEY ? process.env.AMAZON_ACCESS_KEY.substring(0,5)+'...' : 'VIDE',
-    sk: process.env.AMAZON_SECRET_KEY ? 'OK' : 'VIDE',
-    tag: process.env.AMAZON_ASSOCIATE_TAG || 'VIDE'
-  })
-}
