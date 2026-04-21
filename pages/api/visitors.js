b30eb4f50ececeb2d7cbccb846f448015e24209c
Reussitess30 <influenceur@reussitess.fr>
2026-04-21 13:00:55 -0400

    Fix visitors counter

--- pages/api/visitors.js
+++ pages/api/visitors.js
@@ -1,4 +1,14 @@
-export default function handler(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).json({ success: true, count: 6940 });
+import Redis from "ioredis";
+
+const r = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+
+export default async function handler(req, res) {
+  try {
+    const count = await r.incr("reussitess_visitors");
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json({ success: true, count: Number(count) });
+  } catch (error) {
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json({ success: false, count: 0 });
+  }
 }
